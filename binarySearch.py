33f19e4f52f1ceabdf8e0fc00ae4386e1c29bc89
Rooopeswar Kommalapati <roopeshkom@gmail.com>
2016-05-10 00:42:33 -0400

    Undid changes as code has var scope issue. Will refactor later

--- binarySearch.py
+++ binarySearch.py
@@ -1,11 +1,5 @@
 def info(ans, guess, count):
-	print "%d. Answer:%d, Guess:%d" % (count, ans, guess)
-
-def check_guess(guess, ans, lo, hi):
-	if guess > ans:
-		hi = max(lo, ((lo+hi)/2)-1)			
-	else:
-		lo  = min(hi, ((lo+hi)/2)+1)
+	print "%d. Answer:%d, Guess:%d" % (count, ans, guess)	
 
 print "Please enter the lower and upper bounds (ex: 0 99)"
 x = raw_input('> ')
@@ -22,7 +16,10 @@ while True:
 		print "YAY! Finding the answer took me %d guesses." % count
 		break
 	else:
-		check_guess()
+		if guess > ans:
+			hi = max(lo, ((lo+hi)/2)-1)			
+		else:
+			lo  = min(hi, ((lo+hi)/2)+1)
 	count += 1
 
 
